4c161f6342a2d12f8cfb427280649e2bb59fd32b
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2024-11-08 14:08:28 +0200

    Added latest posts on main page and all posts in page all posts

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,10 @@ from datetime import date
 
 from django.shortcuts import render
 
-posts = [
+all_posts = [
     {
         'slug': 'hike-in-the-mountains',
-        'image': 'mountains.jpg',
+        'image': 'mountain.jpg',
         'author': 'Roman',
         'date': date(2023, 11, 20),
         'title': 'Mountain Hiking',
@@ -104,12 +104,21 @@ posts = [
     }
 ]
 
+def get_date(post):
+    return post['date']
+
 # Create your views here.
 def starting_page(request):
-    return render(request, 'blog/index.html')
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, 'blog/index.html', {
+        'posts': latest_posts
+    })
 
 def posts(request):
-    return render(request, 'blog/all-posts.html')
+    return render(request, 'blog/all-posts.html', {
+        "all_posts": all_posts
+    })
 
 def post_detail(request, slug):
     return render(request, 'blog/post-detail.html')
