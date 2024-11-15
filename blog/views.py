3794daf31d503fa3c9affc8d2737116a72b02d9d
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2024-11-15 10:59:01 +0200

    Migrations, added admin page, added database, added models

--- blog/views.py
+++ blog/views.py
@@ -1,127 +1,25 @@
 from datetime import date
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-all_posts = [
-    {
-        'slug': 'hike-in-the-mountains',
-        'image': 'mountain.jpg',
-        'author': 'Roman',
-        'date': date(2023, 11, 20),
-        'title': 'Mountain Hiking',
-        'excerpt': 'There\'s nothing like the views you get when hiking in the mountains!',
-        'content': """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-            
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-            
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-        """
-    },
-    {
-        'slug': 'racing-cars',
-        'image': 'racing.jpg',
-        'author': 'Roman',
-        'date': date(2024, 6, 5),
-        'title': 'Car Racing',
-        'excerpt': 'There\'s nothing better than adrenaline while you race the one of fastest cars in history!',
-        'content': """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-        """
-    },
-    {
-        'slug': 'gym-exercises',
-        'image': 'gym.jpg',
-        'author': 'Roman',
-        'date': date(2007, 12, 29),
-        'title': 'Gym Exercises',
-        'excerpt': 'There\'s nothing better than lift bigger weight from one to next train, bigger, bigger and bigger!',
-        'content': """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Vivamus vitae ante ac dolor suscipit vehicula eget ut dui.
-            Vestibulum ac lobortis massa, ac malesuada dolor. Ut diam sem,
-            convallis pulvinar molestie vel, ullamcorper eu libero. Etiam ac maximus sem,
-            sit amet consectetur felis. Mauris malesuada dignissim justo,
-            eget efficitur augue feugiat a. Nam accumsan lorem mi,
-            quis ornare lectus scelerisque sit amet. Morbi sit amet varius sapien.
-        """
-    }
-]
-
-def get_date(post):
-    return post['date']
+from .models import Post
 
 # Create your views here.
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, 'blog/index.html', {
         'posts': latest_posts
     })
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
         "all_posts": all_posts
     })
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blog/post-detail.html', {
-        'post': identified_post
+        'post': identified_post,
+        'post_tags': identified_post.tags.all()
     })
