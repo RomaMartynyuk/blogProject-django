17a86666657b05a89895cfc07add7ceccbb42757
RomaMartynyuk <95542738+RomaMartynyuk@users.noreply.github.com>
2025-02-17 13:59:37 +0200

    Read Later Starting Setup

--- blog/views.py
+++ blog/views.py
@@ -79,3 +79,7 @@ class SinglePostView(View):
 #         'post': identified_post,
 #         'post_tags': identified_post.tags.all()
 #     })
+
+class ReadLaterView(View):
+    def post(self, request):
+        pass
